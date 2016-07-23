368d10759c6f39e558551c7c43c456f0fffd8775
joey <joeyliu616@live.cn>
2016-07-23 14:00:53 +0800

    修复单元测试, 修改maven 安装方式

--- version-service/src/main/java/com/aoe/service/version/VersionServiceDevConfigHook.java
+++ version-service/src/main/java/com/aoe/service/version/VersionServiceDevConfigHook.java
@@ -1,27 +1,21 @@
 package com.aoe.service.version;
 
+import com.aoe.service.version.config.LiquibaseDevConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
-
-import javax.annotation.PostConstruct;
 
 /**
  * Created by joey on 16-7-20.
  */
 @Configuration
 @Profile({"dev","ut"})
-@EnableAutoConfiguration
-@ComponentScan
+@Import(LiquibaseDevConfig.class)
+@ComponentScan("com.aoe.service.version")
 public class VersionServiceDevConfigHook {
 
     private static Logger logger = LoggerFactory.getLogger(VersionServiceDevConfigHook.class);
-
-    @PostConstruct
-    public void info(){
-        logger.info("--------------- ");
-    }
 }
---
+++ version-service/src/main/java/com/aoe/service/version/config/LiquibaseDevConfig.java
@@ -0,0 +1,7 @@
+package com.aoe.service.version.config;
+
+/**
+ * Created by joey on 16-7-23.
+ */
+public class LiquibaseDevConfig {
+}

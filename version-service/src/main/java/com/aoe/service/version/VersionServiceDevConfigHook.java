86420b47036d712e3a7620cc7f06795d91903ebb
joey <joeyliu616@live.cn>
2016-07-23 14:25:06 +0800

    修复单元测试

--- version-service/src/main/java/com/aoe/service/version/VersionServiceDevConfigHook.java
+++ version-service/src/main/java/com/aoe/service/version/VersionServiceDevConfigHook.java
@@ -3,6 +3,7 @@ package com.aoe.service.version;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile({"dev","ut"})
 @EnableAutoConfiguration
+@ComponentScan
 public class VersionServiceDevConfigHook {
 
     private static Logger logger = LoggerFactory.getLogger(VersionServiceDevConfigHook.class);

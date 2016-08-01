c3339f7b491758fa97c52d1b08e13d2bace07f63
joey <joeyliu616@live.cn>
2016-08-02 00:52:03 +0800

    添加集成测试自动部署脚本

--- integration-test/deploy.sh
+++ integration-test/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 echo "Docker Host == >$DOCKER_HOST"
-
+basepath=$(cd `dirname $0`; pwd)
 # start mysql
-sh ./cleanup.sh mysql
+sh $basepath/cleanup.sh mysql
 docker run --net=overlay --name mysql -e MYSQL_DATABASE=db_version -e MYSQL_ROOT_PASSWORD=root -e constraint:itype==misc -d joey.pc:5000/mysql --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci --default-storage-engine=InnoDB
 
 #start redis
-sh ./cleanup.sh redis
-docker run  --name redis --net=overlay -e constraint:itype==misc --restart=always -d joey.pc:5000/redis
+sh $basepath/cleanup.sh redis
+docker run  --name redis --net=overlay -e constraint:itype==misc -d joey.pc:5000/redis
 
 #start version-service
-docker run -P --net=overlay -e constraint:itype==misc -e spring.profiles.active=it -d joey.pc:5000/version.service
+docker run --net=overlay -e constraint:itype==backend -e spring.profiles.active=it -d joey.pc:5000/version-service
